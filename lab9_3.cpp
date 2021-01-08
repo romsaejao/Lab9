e4392210c3f4cd0e26498d27e2bc363a46a204b0
romsaejao <76035596+romsaejao@users.noreply.github.com>
2021-01-08 19:08:48 +0700

    Lab9 done

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+int main(){
+    int num=0;
+    float m,sd,sum=0,sum2=0;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline))
+    {
+        sum = sum + atof(textline.c_str());
+        sum2 = sum2+ pow(atof(textline.c_str()),2);
+        num++;
+    }
+    m = sum/num;
+    sd = sqrt((sum2/num)-pow(m,2));
+    
+    cout << "Number of data = " << num;
+    cout << setprecision(3);
+    cout << "\nMean = " << m;
+    cout << "\nStandard deviation = " << sd;
+}
